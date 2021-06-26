7251a4b7e6cfea037c357494abec5788eb7cfe7d
Dagnober <dcastaeda704@misena.edu.co>
2021-06-26 18:45:07 -0500

    cargando imagen

--- ASP2184587/Controllers/ProductoController.cs
+++ ASP2184587/Controllers/ProductoController.cs
@@ -3,7 +3,8 @@ using System.Linq;
 using System.Web.Mvc;
 using ASP2184587.Models;
 using Rotativa;
-
+using System.IO;
+using System.Web;
 
 namespace ASP2184587.Controllers
 {
@@ -41,15 +42,40 @@ namespace ASP2184587.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(producto producto)
+        public ActionResult Create(producto producto, HttpPostedFileBase imagen)
         {
             if (!ModelState.IsValid)
                 return View();
+
+            //string para guardar la ruta
+            string filePath = string.Empty;
+
+            //condicion para saber si llego o no el archivo
+            if (imagen != null)
+            {
+                //ruta de la carpeta que caragara el archivo
+                string path = Server.MapPath("~/Uploads/");
+
+                //verificar si la ruta de la carpeta existe
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                //obtener el nombre del archivo
+                filePath = path + Path.GetFileName(imagen.FileName);
+                //obtener la extension del archivo
+                string extension = Path.GetExtension(imagen.FileName);
+
+                //guardando el archivo
+                imagen.SaveAs(filePath);
+            }
 
             try
             {
                 using (var db = new inventarioEntities1())
                 {
+                    producto.imagen = "/Uploads/" + Path.GetFileName(imagen.FileName);
                     db.producto.Add(producto);
                     db.SaveChanges();
                     return RedirectToAction("Index");
